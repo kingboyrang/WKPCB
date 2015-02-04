7824f9f5c79838e9f493552cbfb8a74de81ae2e8
kingboyrang <zhongduan-mini@zhongduan-minideMac-mini.local>
2015-02-04 12:08:38 +0800

    Initial Commit

---
+++ WKPCB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKPCB
+//
+//  Created by wulanzhou-mini on 15-2-4.
+//  Copyright (c) 2015年 wulanzhou-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
